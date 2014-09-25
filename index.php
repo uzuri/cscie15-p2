4c1be2452f2adfd96db9740c3ffdb1735943dcca
Rebecca Mazur <mazurr@rebeccas-mbp.home>
2014-09-25 18:04:23 -0400

    Put the form above the description

--- index.php
+++ index.php
@@ -8,6 +8,16 @@ include("functions.php");
 ?>
 <h1><?php echo $sitetitle; ?></h1>
 
+
+<form method="post" action="index.php">
+<p><label for="words">Number of Words: </label><input name="words" type="number" value="4" min="1" max="20" /></p>
+<p><label for="inc_num">Include a number?</label><input name="inc_num" type="radio" value="Yes" /> Yes | <input name="inc_num" type="radio" value="No" /> No</p>
+<p><label for="inc_char">Include a special character (!,@,#,$,%, etc.)?</label><input name="inc_char" type="radio" value="Yes" /> Yes | <input name="inc_char" type="radio" value="No" /> No</p>
+
+<p><input type="submit" value="Give me another password" /></p>
+</form>
+
+
 <p>XKCD passwords use a style of password generation put forward by comic author/illustrator Randall Munroe in comic 936.  The technique essentially uses words in place of numbers to expand the password entropy while easing rememberability.  But Randall explains it best in the original comic: </p>
 
 <p><a href="http://xkcd.com/936/"><img src="http://imgs.xkcd.com/comics/password_strength.png" /></a></p>
@@ -17,13 +27,6 @@ include("functions.php");
 <p class="pass"><strong>Your Password: </strong></p>
 
 
-<form method="post" action="index.php">
-<p><label for="words">Number of Words: </label><input name="words" type="number" value="4" min="1" max="20" /></p>
-<p><label for="inc_num">Include a number?</label><input name="inc_num" type="radio" value="Yes" /> Yes | <input name="inc_num" type="radio" value="No" /> No</p>
-<p><label for="inc_char">Include a special character (!,@,#,$,%, etc.)?</label><input name="inc_char" type="radio" value="Yes" /> Yes | <input name="inc_char" type="radio" value="No" /> No</p>
-
-<p><input type="submit" value="Give me another password" /></p>
-</form>
 
 <?php
 
