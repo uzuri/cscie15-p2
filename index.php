78f8720b664a77dd6e343493ad4aa3e75483d8ad
Rebecca Mazur <mazurr@rebeccas-mbp.home>
2014-09-25 18:02:56 -0400

    Form controls

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
+
 $sitetitle = "P2: XKCD Password Generator";
-echo $sitetitle;
 
 include("header.inc");
 include("functions.php");
@@ -13,6 +13,17 @@ include("functions.php");
 <p><a href="http://xkcd.com/936/"><img src="http://imgs.xkcd.com/comics/password_strength.png" /></a></p>
 
 <p>This page generates XKCD-style passwords for you to use.  By default it will give you a four-word, all-lowercase passphrasephrase, but you can request different settings below.</p>
+
+<p class="pass"><strong>Your Password: </strong></p>
+
+
+<form method="post" action="index.php">
+<p><label for="words">Number of Words: </label><input name="words" type="number" value="4" min="1" max="20" /></p>
+<p><label for="inc_num">Include a number?</label><input name="inc_num" type="radio" value="Yes" /> Yes | <input name="inc_num" type="radio" value="No" /> No</p>
+<p><label for="inc_char">Include a special character (!,@,#,$,%, etc.)?</label><input name="inc_char" type="radio" value="Yes" /> Yes | <input name="inc_char" type="radio" value="No" /> No</p>
+
+<p><input type="submit" value="Give me another password" /></p>
+</form>
 
 <?php
 
