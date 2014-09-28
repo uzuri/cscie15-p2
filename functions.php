238272d01cc704607f31fc66b8c87584dbde0967
Rebecca Mazur <mazurr@rebeccas-mbp.home>
2014-09-28 18:10:09 -0400

    Try different regex

--- functions.php
+++ functions.php
@@ -35,7 +35,7 @@ function getwords($allwords, $numwords)
 	{
 		$key = array_rand($allwords, 1);
 		$word = $allwords[$key];
-		if (preg_match("/\W/", $word))
+		if (preg_match("/[^a-zA-Z]/", $word))
 		{
 			$words[] = $word;
 			$i++;
